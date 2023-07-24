4f8f8f49ad9db7e2d6ef43b7f9dc685a284e509c
Ghalmas Shanditya Putra Agung <ghalmas.agung@nri.co.id>
2023-07-25 00:27:59 +0700

    fix(): import missing command

--- libs/core/src/commands/index.ts
+++ libs/core/src/commands/index.ts
@@ -4,10 +4,16 @@ import { LoginOrganizerCommand } from './login-organizer/login-organizer.command
 import { LoginOrganizerHandler } from './login-organizer/login-organizer.handler';
 import { LoginUserCommand } from './login-user/login-user.command';
 import { LoginUserHandler } from './login-user/login-user.handler';
+import { OrganizerLogoutCommand } from './organizer-logout/organizer-logout.command';
+import { OrganizerLogoutHandler } from './organizer-logout/organizer-logout.handler';
+import { RegisterOrganizerCommand } from './register-organizer/register-organizer.command';
+import { RegisterOrganizerCodeHandler } from './register-organizer/register-organizer.handler';
 import { RegisterUserCommand } from './register-user/register-user.command';
 import { RegisterUserHandler } from './register-user/register-user.handler';
 import { ResendActivationCodeCommand } from './resend-activation-code/resend-activation-code.command';
 import { ResendActivationCodeHandler } from './resend-activation-code/resend-activation-code.handler';
+import { VerifyUserCommand } from './verify-user/verify-user.command';
+import { VerifyUserHandler } from './verify-user/verify-user.handler';
 
 export {
   LoginUserCommand,
@@ -15,6 +21,9 @@ export {
   CheckAvailabilityEmailCommand,
   RegisterUserCommand,
   ResendActivationCodeCommand,
+  OrganizerLogoutCommand,
+  RegisterOrganizerCommand,
+  VerifyUserCommand,
 };
 
 export const CommandHandlers = [
@@ -23,4 +32,7 @@ export const CommandHandlers = [
   CheckAvailabilityEmailHandler,
   RegisterUserHandler,
   ResendActivationCodeHandler,
+  OrganizerLogoutHandler,
+  RegisterOrganizerCodeHandler,
+  VerifyUserHandler,
 ];
