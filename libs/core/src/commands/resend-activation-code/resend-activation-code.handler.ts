3f1c051d8f0e40073efb406e8b8be5bf19fd0e13
Ghalmas Shanditya Putra Agung <ghalmas.agung@nri.co.id>
2023-07-25 00:48:29 +0700

    fix(): service injection

--- libs/core/src/commands/resend-activation-code/resend-activation-code.handler.ts
+++ libs/core/src/commands/resend-activation-code/resend-activation-code.handler.ts
@@ -1,5 +1,5 @@
+import { AUTH_SERVICE } from '@mulailomba/core/constants';
 import { IAuthService } from '@mulailomba/core/interfaces';
-import { USER_SERVICE } from '@mulailomba/user/constants';
 import { Inject } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
@@ -12,7 +12,7 @@ export class ResendActivationCodeHandler
   constructor(
     @InjectPinoLogger(ResendActivationCodeHandler.name)
     private readonly logger: PinoLogger,
-    @Inject(USER_SERVICE)
+    @Inject(AUTH_SERVICE)
     private readonly authService: IAuthService,
   ) {}
 
