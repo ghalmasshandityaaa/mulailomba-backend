0d696d1df2c8a74d3a59fec0fba145bd565ed831
Ghalmas Shanditya Putra Agung <ghalmas.agung@nri.co.id>
2023-07-30 04:21:50 +0700

    chore(): add file topic validation

--- src/file/file.dto.ts
+++ src/file/file.dto.ts
@@ -2,8 +2,18 @@ import { Transform } from 'class-transformer';
 import * as joi from 'joi';
 import { JoiSchema } from 'joi-class-decorators';
 
+export type FILE_TOPIC_TYPE = 'UPLOADS' | 'POSTER' | 'PROFILES' | 'BANNERS' | 'TIMELINES';
 export class UploadFileBodyDTO {
-  @JoiSchema(joi.string().trim().optional().default('UPLOADS').label('topic'))
+  @JoiSchema(
+    joi
+      .string()
+      .trim()
+      .uppercase()
+      .valid('UPLOADS', 'POSTER', 'PROFILES', 'BANNERS', 'TIMELINES')
+      .optional()
+      .default('UPLOADS')
+      .label('topic'),
+  )
   @Transform(({ value }) => value.toUpperCase())
   readonly topic: string;
 }
