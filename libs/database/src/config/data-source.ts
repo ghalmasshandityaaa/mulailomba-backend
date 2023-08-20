d710d45b6cd516f03d116570bbc2c07e96b63d5a
Ghalmas Shanditya Putra Agung <ghalmas.agung@nri.co.id>
2023-08-20 17:52:08 +0700

    ci(): add database ssl configuration

--- libs/database/src/config/data-source.ts
+++ libs/database/src/config/data-source.ts
@@ -12,4 +12,10 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.PG_DATABASE_URL,
   migrations: [...Migrations, ...Seeds],
+  ssl:
+    process.env.PG_DATABASE_SSL === 'true'
+      ? {
+          rejectUnauthorized: false,
+        }
+      : false,
 });
