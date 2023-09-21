ebd86e27d4b01928f65068e2a94ce56c5f43cda7
Ghalmas Shanditya Putra Agung <ghalmas.agung@nri.co.id>
2023-09-22 01:36:13 +0700

    feat(): enable helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,7 +13,8 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { json } from 'express';
-import { Logger as PinoLogger } from 'nestjs-pino';
+import helmet from 'helmet';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -22,9 +23,16 @@ async function bootstrap() {
   });
 
   const config = app.get(ServerConfigService);
-  app.useLogger(app.get(PinoLogger));
+  const logger = app.get(Logger);
+  app.useLogger(logger);
   app.use(json({ limit: '5mb' }));
   app.use(cookieParser());
+  app.use(
+    helmet({
+      contentSecurityPolicy: true,
+      hidePoweredBy: true,
+    }),
+  );
   app
     .useGlobalPipes(
       new ValidationPipe({
@@ -51,7 +59,9 @@ async function bootstrap() {
       methods: ['GET', 'POST'],
     });
 
-  console.log(123, config);
-  await app.listen(config.port || 3000);
+  await app.startAllMicroservices();
+  await app.listen(config.port || 3000, () => {
+    logger.log(`API service running on port ${config.port}...`);
+  });
 }
 bootstrap();
